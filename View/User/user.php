e0379037ae61b5effcc60e68116aa508588d2c7a
lamkbvn <lamkbvn@gmai.com>
2024-03-05 20:20:20 +0700

    lam

--- View/User/user.php
+++ View/User/user.php
@@ -133,16 +133,7 @@
     </div>
 
     <div class="display-detail">
-      <?php
-      if (isset($_GET['pageuser'])) {
-        if ($_GET['pageuser'] == 'mud')
-          include("./View/User/MucUuDai.php");
-        elseif ($_GET['pageuser'] == 'dh')
-          include("./View/User/DonHang.php");
-        elseif ($_GET['pageuser'] == 'dg')
-          include("./View/User/DanhGia.php");
-      }
-      ?>
+      <?php include("Model/User/displayDetail.php"); ?>
     </div>
   </section>
 </body>
