3f68a39958166e76915034b58defb05972b54cc3
lamkbvn <lamkbvn@gmai.com>
2024-03-24 00:10:45 +0700

    lam

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-include ('Model/DBConfig.php');
-$db = new DBConfig();
+// include ('Model/DBConfig.php');
+// $db = new DBConfig();
 // $db->connect();
 // $db->foreignKey('order_detail', 'id_product', 'product', 'id');
 // $db->disconnect();
 // include('Controller/User.php');
 // include('View/User/user.php');
-include ('Controller/ThongKe.php');
-include ('View/ThongKe/thongke.php');
+// include ('Controller/ThongKe.php');
+// include ('View/ThongKe/thongke.php');
 ?>
