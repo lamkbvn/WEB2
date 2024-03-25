b43392c0bfa45ee3165a6cf8c25b289beb0ff4e6
Duy Lam <duylam468213@gmail.com>
2024-03-25 14:12:27 +0700

    d

--- Controller/showproductController/showproduct.php
+++ Controller/showproductController/showproduct.php
@@ -1,14 +1,11 @@
-<?php 
-    include "../../../../Model/DBConfig.php";
-    $db = new Database;
-    $db->connect();
-    $table = 'product';
-    // $result = $db->getAllData($table);
-    $listCategory = $db->getAllData('category');
-    $page = 1;
-    if(isset($_GET['keyword'])) {
-        
-    }
-    require_once("View/showproductView/index.php");
-
-?>
+<?php
+// include "../../../../Model/DBConfig.php";
+// $db = new Database;
+// $db->connect();
+$table = 'product';
+// $result = $db->getAllData($table);
+$listCategory = $db->getAllData('category');
+$page = 1;
+if (isset($_GET['keyword'])) {
+}
+require_once("View/showproductView/index.php");
