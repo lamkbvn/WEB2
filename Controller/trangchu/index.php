a2a42ac0cab394f129233dff81c418ed007e8396
Duy Lam <duylam468213@gmail.com>
2024-03-25 13:51:00 +0700

    d

--- Controller/trangchu/index.php
+++ Controller/trangchu/index.php
@@ -1,5 +1,5 @@
 <?php
-if (isset ($_GET['action'])) {
+if (isset($_GET['action'])) {
 	$action = $_GET['action'];
 } else {
 	$action = '';
@@ -8,45 +8,45 @@ if (isset ($_GET['action'])) {
 
 switch ($action) {
 	case 'shoping': {
-		if (isset ($_POST['login'])) {
-			header('location: index.php?controller=trang-admin&action=indexAdmin');
-		}
-		require_once ('View/trangchu/shoping.php');
-		break;
-	}
-
-	case 'login': {
-		session_start();
-		ob_start();
-		if ($_SERVER["REQUEST_METHOD"] == "POST" && isset ($_POST['login'])) {
-			$username = $_POST['username'];
-			$password = $_POST['password'];
-			if ($db->checkLogin($username, $password)) {
-				$objuser = array($username, $password);
-				$_SESSION['objuser'] = $objuser;
-				header('Location: index.php?controller=trang-chu');
-				exit();
-			} else {
-				echo "Tên người dùng hoặc mật khẩu không đúng.";
+			if (isset($_POST['login'])) {
+				header('location: index.php?controller=trang-admin&action=indexAdmin');
 			}
-		}
-		require_once ('View/trangchu/login.php');
-		break;
-	}
-	case "showproduct": // cua Huu Loc
-		require_once ('Controller/showproductController/showproduct.php');
-		break;
-	case "userprofile":
-		require_once ('View/User/user.php');
-		break;
-	default: {
-		if (isset ($db)) {
-			$listUsersTable = "product";
-			$dataHotProduct = $db->getAllData($listUsersTable);
-			$listCategory = $db->getAllData("category");
-
-			require_once ('View/trangchu/trangchu.php');
+			require_once('View/trangchu/shoping.php');
 			break;
 		}
-	}
+
+	case 'login': {
+			session_start();
+			ob_start();
+			if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['login'])) {
+				$username = $_POST['username'];
+				$password = $_POST['password'];
+				if ($db->checkLogin($username, $password)) {
+					$objuser = array($username, $password);
+					$_SESSION['objuser'] = $objuser;
+					header('Location: index.php?controller=trang-chu');
+					exit();
+				} else {
+					echo "Tên người dùng hoặc mật khẩu không đúng.";
+				}
+			}
+			require_once('View/trangchu/login.php');
+			break;
+		}
+	case "showproduct": // cua Huu Loc
+		require_once('Controller/showproductController/showproduct.php');
+		break;
+	case "userprofile":
+		require_once('View/User/user.php');
+		break;
+	default: {
+			if (isset($db)) {
+				$listUsersTable = "product";
+				$dataHotProduct = $db->getAllData($listUsersTable);
+				$listCategory = $db->getAllData("category");
+
+				require_once('View/trangchu/trangchu.php');
+				break;
+			}
+		}
 }
