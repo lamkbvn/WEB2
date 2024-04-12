1fbc64d9fcaf44aa3de300ffbbac7cd2991478fa
Duy Lam <duylam468213@gmail.com>
2024-04-12 09:40:06 +0700

    fix add user

--- Controller/trangadmin/ChucNang/C_editChucNang.php
+++ Controller/trangadmin/ChucNang/C_editChucNang.php
@@ -1,15 +1,14 @@
-<?php 
+<?php
 include("../../../Model/DBConfig.php");
 $db = new Database();
 $db->connect();
-if(isset($_REQUEST['nameCN'])){
+if (isset($_REQUEST['nameCN'])) {
     $name = $_REQUEST['nameCN'];
     $id = $_REQUEST['id'];
     $result = $db->UpdateNameChucNang($id, $name);
-    if($result !== false) {
+    if ($result != false) {
         echo "<script>alert('Sửa thành công');</script>";
     }
 }
 
 echo "<script>window.location.href = '/WEB2/index.php?controller=trang-admin&action=chucnang';</script>";
-?>
--- includes/handle_mail.php
+++ includes/handle_mail.php
@@ -3,7 +3,6 @@ require_once(__DIR__ . "/phpmailer/Exception.php");
 require_once(__DIR__ . "/phpmailer/PHPMailer.php");
 require_once(__DIR__ . "/phpmailer/SMTP.php");
 require_once(__DIR__ . "/function.php");
-// include "../Model/DBConfig.php";
 
 // Kiểm tra xem có dữ liệu được gửi qua phương thức POST không
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email'])) {
